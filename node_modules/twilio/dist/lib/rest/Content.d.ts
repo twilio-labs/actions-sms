e22b7428139fe312017be420b97852ef736bf8b8
Tierney Cyren <hello@bnb.im>
2024-04-30 10:48:43 -0400

    chore: update dependencies

--- node_modules/twilio/dist/lib/rest/Content.d.ts
+++
@@ -1,9 +0,0 @@
-import ContentBase from "./ContentBase";
-import { ContentListInstance } from "./content/v1/content";
-declare class Content extends ContentBase {
-    /**
-     * @deprecated - Use v1.contents instead
-     */
-    get contents(): ContentListInstance;
-}
-export = Content;
