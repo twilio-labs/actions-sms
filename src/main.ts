024c5422c7b799126f2adc043c494e456a659f67
Shivani Brijmohan <36177801+sbrij001@users.noreply.github.com>
2024-08-23 13:16:06 -0400

    Updated Main.ts Authentication and Env variables (#71)
    Updated Main.ts Authentication and Env variables

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,19 @@ const core = require('@actions/core');
 const twilio = require('twilio');
 
 async function run() {
-  const from = core.getInput('fromPhoneNumber');
-  const to = core.getInput('toPhoneNumber');
-  const message = core.getInput('message');
+  const from = core.getInput('fromPhoneNumber') || process.env.FROM_PHONE_NUMBER;
+  const to = core.getInput('toPhoneNumber') || process.env.TO_PHONE_NUMBER;
+  const message = core.getInput('messageToSend') || process.env.MESSAGE_TO_SEND;
 
+  
   const accountSid =
     core.getInput('TWILIO_ACCOUNT_SID') || process.env.TWILIO_ACCOUNT_SID;
-  const apiKey = core.getInput('TWILIO_API_KEY') || process.env.TWILIO_API_KEY;
-  const apiSecret =
-    core.getInput('TWILIO_API_SECRET') || process.env.TWILIO_API_SECRET;
+  const authToken =
+    core.getInput('TWILIO_AUTH_TOKEN') || process.env.TWILIO_AUTH_TOKEN;
 
-  core.debug('Sending SMS');
-  const client = twilio(apiKey, apiSecret, { accountSid });
+  core.debug('Authenticating with Twilio');
+  const client = twilio(accountSid, authToken);
+  core.debug('Attempting to send SMS!');
   const resultMessage = await client.messages.create({
     from,
     to,
